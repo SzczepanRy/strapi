b9adbdc1e300540f9dbbd481fc574f06817ea116
unknown <szep.rydzewski@gmail.com>
2023-05-17 14:39:45 +0200

    s

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,10 @@
 module.exports = ({ env }) => ({
   "netlify-deployments": {
     enabled: true,
+    config: {
+      name: "Site",
+      id: "65b6241f-2dbb-4ab1-a3ef-7cc7b2ff4e18",
+      buildHook: "https://api.netlify.com/build_hooks/6464caee77e73031e44e1dfb",
+    },
   },
 });
