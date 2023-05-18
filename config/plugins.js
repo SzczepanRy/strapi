059b2c96f6ce99264af936e0695b3824c28f4860
unknown <szep.rydzewski@gmail.com>
2023-05-18 15:27:12 +0200

    aaa

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,15 @@ module.exports = ({ env }) => ({
   "netlify-deployments": {
     enabled: true,
     config: {
-      name: "boisterous-tartufo-27f13f",
-      id: "65b6241f-2dbb-4ab1-a3ef-7cc7b2ff4e18",
-      buildHook: "https://api.netlify.com/build_hooks/6464caee77e73031e44e1dfb",
+      //   accessToken: "<netlify-access-token>",
+      sites: [
+        {
+          name: "boisterous-tartufo-27f13f",
+          id: "65b6241f-2dbb-4ab1-a3ef-7cc7b2ff4e18",
+          buildHook:
+            "https://api.netlify.com/build_hooks/6464caee77e73031e44e1dfb",
+        },
+      ],
     },
   },
 });
